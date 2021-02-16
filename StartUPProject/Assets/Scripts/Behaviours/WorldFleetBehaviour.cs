569ed218b8c5ef82b7e6dfb80a30619dcc8ad9b5
Matioma <Artiom.Matievschi@gmail.com>
2021-02-16 10:50:51 +0100

    Fixx project

---
+++ StartUPProject/Assets/Scripts/Behaviours/WorldFleetBehaviour.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WorldFleetBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    public WorldFleetData worldFleetData;
+
+    public bool Selected=false;
+
+    public UnityEvent onSelect;
+    public UnityEvent onDeselect;
+
+    public void Init(WorldFleetData data) {
+        worldFleetData = data;
+    }
+
+
+
+    public void SelectingShip() {
+        if (worldFleetData.isPlayer) return;
+        if (Selected) return;
+        
+        ParentDeselectAllShips();
+        onSelect?.Invoke();
+        Selected = true;
+    }
+
+    public void DeselectShip() {
+        if (worldFleetData.isPlayer) return;
+        if (!Selected) return;
+
+        onDeselect?.Invoke();
+        Selected = false;
+    }
+
+
+    public void ParentDeselectAllShips()
+    {
+        GetComponentInParent<WorldModel>().DeselectAllShips();
+    }
+
+    public void AttackShip() {
+        Debug.Log("//To Do Attack a ship");
+    }
+}
---
+++ StartUPProject/Assets/Scripts/Views/WorldShipView.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WorldShipView : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI name;
+
+    [SerializeField]
+    TextMeshProUGUI level;
+
+    private void Start()
+    {
+        Display();
+    }
+
+
+    void Display() {
+        WorldFleetBehaviour data = GetComponentInParent<WorldFleetBehaviour>();
+
+        if (data.worldFleetData.isPlayer)
+        {
+            name.text = "Your Ship";
+        }
+        else {
+            name.text = "Player" + Random.Range(1, 100);
+        }
+
+    }
+
+}
