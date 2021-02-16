761ee8721c735127877fe8eae1bd808147ca179b
Matioma <Artiom.Matievschi@gmail.com>
2021-02-16 13:06:45 +0100

    Add resources on win

---
+++ StartUPProject/Assets/Scripts/VictoryResources.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryResources : MonoBehaviour
+{
+    [SerializeField]
+    FleetModel fleetModel;
+
+
+    [SerializeField]
+    int gold;
+    public int Gold => gold;
+    
+    [SerializeField]
+    int wood;
+    public int Wood => wood;
+    
+    [SerializeField]
+    int oranges;
+    public int Oranges => oranges;
+
+    private void OnEnable()
+    {
+        gold = Random.Range(10, 100) * 10;
+        wood = Random.Range(10, 100) * 10;
+        oranges = Random.Range(10, 100) * 10;
+    }
+
+
+    public void SendResources() {
+        fleetModel.FleetResources.AddResource(ResourceType.Gold, gold);
+        fleetModel.FleetResources.AddResource(ResourceType.Oranges, oranges);
+        fleetModel.FleetResources.AddResource(ResourceType.Wood, wood);
+    }
+}
---
+++ StartUPProject/Assets/Scripts/Views/VictoryView.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class VictoryView : MonoBehaviour
+{
+    [SerializeField]
+    VictoryResources resources;
+    [SerializeField]
+    TextMeshProUGUI gold;
+    [SerializeField]
+    TextMeshProUGUI wood;
+    [SerializeField]
+    TextMeshProUGUI oranges;
+
+
+    private void OnEnable()
+    {
+        gold.text = resources.Gold.ToString() + "Gold";
+        wood.text = resources.Wood.ToString() + "Wood";
+        oranges.text = resources.Oranges.ToString() + "Oranges";
+    }
+}
