a1d43739d35e3181deb3ad7dec13216d2f8596c4
Matioma <Artiom.Matievschi@gmail.com>
2021-02-17 15:21:34 +0100

    Deselecting the ships

--- StartUPProject/Assets/Scripts/Select.cs
+++ StartUPProject/Assets/Scripts/Select.cs
@@ -2,41 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class Select : MonoBehaviour
 {
     [SerializeField]
     UnityEvent onSelect;
+    [SerializeField]
+    UnityEvent onDeselect;
 
+    WorldFleetBehaviour selectedShip;
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began){
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
             SelectObject();
         }
-        if (Input.GetMouseButtonDown(0)){
+
+        //Debug.Log(IsMouseOverUI());
+
+        if (Input.GetMouseButtonDown(0))
+        {
             SelectObject();
         }
+
+        //if (Input.GetMouseButtonDown(0) == true && EventSystem.current.IsPointerOverGameObject())
+        //{
+        //    if (EventSystem.current.currentSelectedGameObject.gameObject.tag == "Button") {
+        //        Debug.Log(EventSystem.current.currentSelectedGameObject.gameObject.tag);
+        //    }
+        //    //Debug.Log(EventSystem.current.currentSelectedGameObject.gameObject.name);
+        //}
     }
+
+    bool IsMouseOverUI() {
+        return EventSystem.current.IsPointerOverGameObject();
+    
+    }
+
+    public void AttackSelectedShip() {
+        selectedShip.GetComponent<WorldFleetController>().AttackShip();
+    }
+
 
     void SelectObject()
     {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //if (EventSystem.current.IsPointerOverGameObject()) { 
+        //    if (EventSystem.current.currentSelectedGameObject.gameObject.tag == "Button")
+        //    {
+        //        //Debug.Log(EventSystem.current.currentSelectedGameObject.gameObject.tag);
+        //        return;
+        //    }
+        //}
+
         if (Physics.Raycast(ray, out hit, 100.0f))
         {
             Transform clickedTransform = hit.transform;
             if (clickedTransform == null) return;
 
 
+
+
             WorldFleetBehaviour worldFleetBehaviour = clickedTransform.GetComponent<WorldFleetBehaviour>();
             if (worldFleetBehaviour != null)
             {
-                worldFleetBehaviour.SelectingShip();
-            }else {
-                GetComponentInChildren<WorldModel>()?.DeselectAllShips();
+                if (selectedShip != null) selectedShip.DeselectShip();
+
+                onSelect?.Invoke();
+                selectedShip = worldFleetBehaviour;
+                selectedShip.SelectingShip();
+                return;
             }
+            else
+            {
+                //GetComponentInChildren<WorldModel>()?.DeselectAllShips();
+            }
+
+        }
+        else
+        {
+            if (EventSystem.current.IsPointerOverGameObject()) {
+                if (EventSystem.current.currentSelectedGameObject.gameObject.layer == 5) {
+                    return;
+                }
+            }
+            selectedShip = null;
+            onDeselect?.Invoke();
         }
     }
 }
